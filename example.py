b076a32033c9741d967e4cdd5bb47147d15f32cf
WangChen100 <wangchen100@163.com>
2019-05-20 14:11:09 +0800

    modify example code

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import gym
 import gym_pathfinding
 from time import sleep
 
-env = gym.make('pathfinding-obstacle-25x25-v0')
+env = gym.make('partially-observable-pathfinding-maze-9x9-d10-v0')
 env.seed(1) # for full-deterministic environment
 
 for episode in range(5):
@@ -12,8 +12,10 @@ for episode in range(5):
         env.render()
         sleep(0.05)
         
-        s, r, done, _ = env.step(env.action_space.sample())
-        
+        s, r, done, _, label = env.step(env.action_space.sample())
+
+        print('action label:', label)
+
         if done:
             break
 
